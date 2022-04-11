a867c64d733f5809e4d7665c9bc88e971544c485
stnganyani <85542837+coolhands-stn@users.noreply.github.com>
2022-04-11 22:04:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def predict():
     return render_template('index.html',index=image_index, class_name=frame_class)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
     
